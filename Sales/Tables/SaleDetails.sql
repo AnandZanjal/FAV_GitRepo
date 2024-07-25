aafc14f9eb966ace28dfa93db507a92887285441
AnandZanjal <anand.zanjal@incredotech.com>
2024-07-25 18:04:12 +0530

    first commit

---
+++ Sales/Tables/SaleDetails.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [Sales].[SaleDetails] (
+    [SaleID]      INT           NOT NULL,
+    [ProductID]   VARCHAR (100) NULL,
+    [ProductName] VARCHAR (100) NULL,
+    [Salesrep]    VARCHAR (100) NULL
+);
+GO
+
